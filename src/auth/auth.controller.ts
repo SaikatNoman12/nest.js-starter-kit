b1f3ec02544aceba82356ac40b3020ca32f32364
Abdullah Al Nomaan <83067643+SaikatNoman12@users.noreply.github.com>
2025-06-28 15:53:35 +0600

    implement active user get data in auth header.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
+import { AllowAnonymous } from './decorators/allow-anonymous.decorator';
 
 @Controller('auth')
+@AllowAnonymous()
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
