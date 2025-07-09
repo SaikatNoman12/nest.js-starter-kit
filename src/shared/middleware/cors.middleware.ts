309b65a87f78495a6fcc84e3286ffb8950b8bf6a
Abdullah Al Nomaan <83067643+SaikatNoman12@users.noreply.github.com>
2025-07-10 02:33:42 +0600

    update middelware res header add cookie

--- src/shared/middleware/cors.middleware.ts
+++ src/shared/middleware/cors.middleware.ts
@@ -14,7 +14,10 @@ export class CorsMiddleware implements NestMiddleware {
       'Access-Control-Allow-Methods',
       'GET, POST, PUT, DELETE, OPTIONS',
     );
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header(
+      'Access-Control-Allow-Headers',
+      'Content-Type, Authorization, Cookie',
+    );
     res.header('Access-Control-Allow-Credentials', 'true'); // needed for credentials
     if (req.method === 'OPTIONS') {
       return res.status(204).end(); // respond to preflight requests
